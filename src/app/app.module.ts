8668803c8e765131993582f831cb7f7a60530dfa
dom-sbn <dom@simplebutneeded.com>
2021-09-16 07:58:41 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
+import { HelloSectionComponent } from './components/hello-section/hello-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainContainerComponent,
+    HelloSectionComponent
   ],
   imports: [
     BrowserModule,
